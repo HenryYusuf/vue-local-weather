66aecef6cd6cba42c445b878dd9698f1211572d1
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2024-04-01 22:00:11 +0700

    local weather with learning vitest

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import GetLocation from "./components/GetLocation.vue";
+</script>
+
+<template>
+  <GetLocation />
+</template>
---
+++ src/components/WindDirection.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+interface Props {
+  degrees: number;
+}
+
+const props = defineProps<Props>();
+const windStyle = computed(() => ({
+  transform: "rotate(" + props.degrees + "deg)",
+}));
+</script>
+
+<template>
+  <span>
+    <span
+      aria-hidden="true"
+      class="inline-block"
+      data-testid="direction"
+      :style="windStyle"
+      >⬇️</span
+    >
+    <span class="sr-only" data-testid="direction-sr"
+      >Wind direction: {{ degrees }} degrees</span
+    >
+  </span>
+</template>
